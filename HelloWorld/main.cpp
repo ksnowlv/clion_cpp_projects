7811bbb20741213a4eba8c0cb0962fb013564852
ksnowlv <lvwei@sogou-inc.com>
2024-05-15 18:55:52 +0800

    添加内存问题，使用valgrind定位查找问题

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,7 +6,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    std::string filePath = "/tmp/linux-sdk/counterlib/video/3/1.mp4";
+
+
     std::cout << "Hello world!" << std::endl;
     unique_ptr<TestCase> tc(make_unique<TestCase>());
     tc->TestMyCase();
+
+    int * p = new int[100];
+
+    p[100] = 3;
 }
